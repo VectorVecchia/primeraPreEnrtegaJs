8a6d9f8b51666ae9aba4b8e4d6f930f4be3b9d64
Franco <mu-zion@hotmail.com>
2023-03-03 12:04:00 -0300

    Primera entrega js-Pokemon

---
+++ script/main.js
@@ -0,0 +1,94 @@
+alert("Weedle Salvaje ha aparecido!");
+let pokemon1 = +prompt("Elige un pokemon: 1: Pikachu  2: Charmander  3: Bulbasaur");
+let pokemon2 = 'Charmander';
+let pokemon3 = 'Bulbasour';
+let life = 100;
+let attack = 30;
+let weedleLife = 100;
+let weedleAttack= 15;
+
+
+if (pokemon1 === 1) {
+    pokemon1 = 'Pikachu';
+    alert(pokemon1 + " Yo te elijo!!!");
+    alert("Derrota al Enemigo ! !");
+    
+    let firstAttack = +prompt("Elige un ataque: 1: Impactrueno  2: Gruñido  3: Ataque Rápido");
+    
+    switch (firstAttack) {
+        case 1: alert('Pikachu usa Impactrueno')
+            break;
+        case 2: alert('Pikachu usa Gruñido')
+            break;
+        case 3: alert('Pikachu usa Ataque Rápido')
+            break;
+        default:
+            alert('Pikachu queda dormido');
+            break;
+    }
+   atacar(pokemon1);
+
+} else if (pokemon1 === 2) {
+    pokemon1 = pokemon2;
+    alert(pokemon2 + " Yo te elijo!!!")
+    alert("Derrota al Enemigo ! !");
+    let firstAttack = +prompt("Elige un ataque: 1: Pantalla de Humo  2: Furia Dragón  3: Ascuas");
+    switch (firstAttack) {
+        case 1: alert('Charmamder usa Pantalla de Humo')
+            break;
+        case 2: alert('Charmamder usa Furia Dragón')
+            break;
+        case 3: alert('Charmamder usa Ascuas')
+            break;
+        default:
+            alert('Charmander queda dormido');
+            break;
+    }
+    atacar(pokemon2);
+
+} else {
+    pokemon1 = pokemon3;
+    alert(pokemon3 + " Yo te elijo!!!")
+    alert("Derrota al Enemigo ! !");
+    let firstAttack = +prompt("Elige un ataque: 1: Placaje  2: Drenadoras  3: Látigo Cepa");
+    switch (firstAttack) {
+        case 1: alert('Bulbasour usa Placaje')
+            break;
+        case 2: alert('Bulbasour usa Drenadoras')
+            break;
+        case 3: alert('Bulbasour usa Látigo Cepa')
+            break;
+        default:
+            alert('Bulbasour queda dormido');
+            break;
+    }
+    atacar(pokemon3);
+}
+
+//    FUNCIÓN PARA ATACAR
+
+
+function atacar() {
+
+
+
+    do {
+        alert('Weedle Ataca causando ' + weedleAttack + ' de daño!');
+        life = life - weedleAttack;
+        alert('La vida de ' + pokemon1 + ' es : ' + life);
+        alert(pokemon1+ ' Ataca causando ' + attack + ' de daño!');
+        weedleLife = weedleLife - attack;
+        alert('La vida de Weedle es :' + weedleLife);
+       
+        // if (charizardLife <0){
+        //     break;
+        // }
+
+    } while (weedleLife > 0);
+     
+    alert('Weedle Derrotado');
+    alert('Fin del juego !');
+}
+
+
+
